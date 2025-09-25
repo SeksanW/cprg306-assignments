6c80cc23b64aa8904ec785e02c6a80f19c872652
SeksanW <164899312+SeksanW@users.noreply.github.com>
2025-09-25 09:24:33 -0600

    created a page.js

---
+++ app/week-3/page.js
@@ -0,0 +1,11 @@
+import Item from "./item";
+import ItemList from "./item-list";
+
+export default function Page(){
+    return(
+        <main>
+            <h1 className="text-5xl m-5 text-blue-400">Shopping List</h1>
+            <ItemList />
+        </main>
+    )
+}
