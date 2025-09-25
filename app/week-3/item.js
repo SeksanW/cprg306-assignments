dd5919aae061db082af146a3544f863f7b1b3dc9
SeksanW <164899312+SeksanW@users.noreply.github.com>
2025-09-25 09:11:58 -0600

    week 3 start
    starting week 3 work

---
+++ app/week-3/item.js
@@ -0,0 +1,15 @@
+
+export default function Item({item}){
+
+    let {name, quantity, category} = item
+
+    return(
+        <li className="bg-blue-300 
+                        text-gray-600
+                        border-2 
+                        border-cyan-300">
+            <h3 className="text-2xl font-bold">{name}</h3>
+            <p>Buy {quantity} in {category}</p>
+        </li>
+    )
+}
