09bae0a8fe6a4a4d73e980b755dd8a94b6639d3d
SeksanW <164899312+SeksanW@users.noreply.github.com>
2025-09-25 09:39:21 -0600

    fic item file

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,16 +1,11 @@
-import Item from "./item-list";
+export default function Item({ item }) {
 
-export default function Item({item}){
-
-    let {name, quantity, category} = item
-
-    return(
-        <li className="bg-blue-300 
-                        text-gray-600
-                        border-2 
-                        border-cyan-300">
+    let { name, quantity, category } = item;
+    return (
+        <li className="bg-blue-300 text-gray-600 border-2 border-cyan-300">
             <h3 className="text-2xl font-bold">{name}</h3>
             <p>Buy {quantity} in {category}</p>
         </li>
-    )
+    );
+    
 }
