032062538f26f114cd7a8d5201638d476d0fa5cf
SeksanW <164899312+SeksanW@users.noreply.github.com>
2025-09-26 15:27:07 -0600

    styling

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -3,14 +3,14 @@ export default function Item({ item }) {
     let { name, quantity, category } = item;
     return (
         <li className="bg-slate-900
-                    border-2
+                    
 
                     w-2xl
-                    m-10
+        
                     p-2
                     ">
         <h3 className="text-2xl font-bold text-white">{name}</h3>
-        <p className="text-gray-600 mt-1">Buy {quantity} in {category}</p>
+        <p className="text-white mt-1">Buy {quantity} in {category}</p>
     </li>
     );
 
