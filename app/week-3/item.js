1aaa67063f32243e38e07b001d245de00dfd7c05
SeksanW <164899312+SeksanW@users.noreply.github.com>
2025-09-26 15:34:44 -0600

    Update item.js

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -3,7 +3,7 @@ export default function Item({ item }) {
     let { name, quantity, category } = item;
     return (
         <li className="bg-slate-900
-                    w-1.5
+                    w-24
                     p-2
                     ">
         <h3 className="text-2xl font-bold text-white">{name}</h3>
