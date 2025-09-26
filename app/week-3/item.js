3cce74ac6d9e926af957667cb08736bc949ef025
SeksanW <164899312+SeksanW@users.noreply.github.com>
2025-09-25 18:47:11 -0600

    Update item.js

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -3,13 +3,8 @@ export default function Item({ item }) {
     let { name, quantity, category } = item;
     return (
         <li className="bg-blue-300 
-                        text-gray-600
-                        border-2 
-                        border-cyan-300
-                        w-2xl
-                        m-5 
-                        p-5 
-                        rounded">
+                        text-white-600
+                        w-2xl">
             <h3 className="text-2xl font-bold">{name}</h3>
             <p>Buy {quantity} in {category}</p>
         </li>
