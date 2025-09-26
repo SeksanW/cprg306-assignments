042704f24cbe336b91e46fe48ecf8cc8324a5b90
SeksanW <164899312+SeksanW@users.noreply.github.com>
2025-09-25 19:06:37 -0600

    styling changes

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,12 +2,11 @@ export default function Item({ item }) {
 
     let { name, quantity, category } = item;
     return (
-        <li className="bg-gray-100  
-                    border border-gray-300 
-                    rounded-md
+        <li className="bg-white/100
+                    border border-white 
                     p-4
                     w-full
-                    max-w-2xl
+                    max-w-md
                     shadow-sm">
         <h3 className="text-xl font-bold text-gray-800">{name}</h3>
         <p className="text-gray-600 mt-1">Buy {quantity} in {category}</p>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -6,7 +6,7 @@ export default function Page(){
         <main>
             <h1 className="text-5xl 
             m-5 
-            text-blue-400">Shopping List</h1>
+            text-white">Shopping List</h1>
             <ItemList />
         </main>
     )
