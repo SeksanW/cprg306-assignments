3b9f3b51c1a79731a101152e2b45a9af92eeb142
SeksanW <164899312+SeksanW@users.noreply.github.com>
2025-10-01 16:41:52 -0600

    Update page.js

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -4,8 +4,9 @@ import NewItem from "./new-item";
 
 export default function Page() {
     return (
-        <main>
+        <main className="min-h-screen flex items-center justify-center bg-gray-50">
             <NewItem />
         </main>
     );
 }
+
