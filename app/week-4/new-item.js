276916996a924fedce8ec47b3aae7ee411c88e46
SeksanW <164899312+SeksanW@users.noreply.github.com>
2025-10-02 18:43:03 -0600

    Update new-item.js

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -34,8 +34,6 @@ return (
     <main className="flex justify-center">
         
         <div className="bg-blue-100 w-50 h-20 p-2 flex flex-col items-center justify-center px-2 shadow space-y-2">
-        {}
-        {}
         <div className="flex items-center justify-between w-full px-6">
         <div className="w-16 h-12 flex items-center justify-center border-2 border-gray-400 rounded bg-white text-lg font-bold text-gray-800">
             {quantity}
@@ -46,7 +44,6 @@ return (
                 disabled={quantity <= 1}
                 className={decrementStyles}
                 > - </button>
-
             <button
                 onClick={increment}
                 disabled={quantity >= 20}
