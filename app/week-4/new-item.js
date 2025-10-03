12e13b8b598143d38a80130a7653789348334acc
SeksanW <164899312+SeksanW@users.noreply.github.com>
2025-10-03 10:25:28 -0600

    Update new-item.js

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -35,7 +35,7 @@ return (
         
         <div className="bg-blue-100 w-50 h-15 p-2 flex flex-col items-center justify-center px-2 shadow space-y-3">
         <div className="flex items-center justify-between w-full px-6">
-        <div className="w-16 h-12 flex items-center justify-center border-2 border-gray-400 rounded bg-white text-lg font-bold text-gray-800 space-y-3">
+        <div className="w-16 h-12 p-2  flex items-center justify-center border-2 border-gray-400 rounded bg-white text-lg font-bold text-gray-800 ">
             {quantity}
         </div>
         <div className="flex space-x-2">
