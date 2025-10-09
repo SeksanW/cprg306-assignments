533911cde3511699299b294b085eee19fdd27386
SeksanW <164899312+SeksanW@users.noreply.github.com>
2025-10-09 08:39:32 -0600

    created week 5

---
+++ app/week-5/new-item.js
@@ -0,0 +1,70 @@
+"use client";
+
+import { useState } from "react";
+
+export default function NewItem() {
+const [quantity, setQuantity] = useState(1);
+
+function newName() {
+    const {Name, setName}   = useState("");
+    const handleChange = (event) => {
+        setName(event.target.value);
+};
+}
+
+const increment = () => {
+    if (quantity < 20) {
+        setQuantity(quantity + 1);
+    }
+};
+
+const decrement = () => {
+    if (quantity > 1) {
+        setQuantity(quantity - 1);
+    }
+};
+
+    const baseButton =
+    "w-10 h-10 flex items-center justify-center rounded text-white text-xl font-bold transition-colors duration-200";
+
+const incrementStyles =
+    quantity >= 20
+        ? "bg-gray-500 cursor-not-allowed " + baseButton
+        : "bg-blue-500 hover:bg-blue-400 " + baseButton;
+
+    const decrementStyles =
+    quantity <= 1
+        ? "bg-gray-500 cursor-not-allowed " + baseButton
+        : "bg-blue-500 hover:bg-blue-400 " + baseButton;
+
+return (
+    <main className="flex justify-center">
+        
+            <form onSubmit={handleSubmit}>  
+        <label>
+            Name:
+            <input type="text" value={Name} onChange={handleChange} />
+        </label>
+    </form>
+        <div className="bg-blue-100 w-50 h-15 p-2 flex flex-col items-center justify-center shadow">
+        <div className="flex items-center justify-between w-full px-6">
+        <div className="w-16 h-10 flex items-center justify-center border-2 border-gray-400 rounded bg-white text-lg font-bold text-gray-800 ">
+            {quantity}
+        </div>
+        <div className="flex space-x-2 ml-4">
+            <button
+                onClick={decrement}
+                disabled={quantity <= 1}
+                className={decrementStyles}
+                > - </button>
+            <button
+                onClick={increment}
+                disabled={quantity >= 20}
+                className={incrementStyles}
+                > + </button>
+        </div>
+        </div>
+        </div>
+    </main>
+);
+}
---
+++ app/week-5/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import NewItem from "./new-item";
+
+export default function Page() {
+    return (
+        <div className="flex items-center 
+                        pt-1
+                        justify-center">
+            <NewItem />
+        </div>
+    );
+}
+
