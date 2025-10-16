56fdb980cad598acd43bacb120e48912d346313f
SeksanW <164899312+SeksanW@users.noreply.github.com>
2025-10-16 17:41:23 -0600

    Update item.js

--- app/week-6/item.js
+++ app/week-6/item.js
@@ -1,8 +1,14 @@
 export default function Item({ item }) {
-    const { name, quantity, category } = item;
+
+    let { name, quantity, category } = item;
     return (
-    <li className="text-white">
-        {name} — Buy {quantity} in {category}
+        <li className="bg-slate-900
+                    w-96
+                    p-2
+                    ">
+        <h3 className="text-2xl font-bold text-white">{name}</h3>
+        <p className="text-white mt-1">Buy {quantity} in {category}</p>
     </li>
-);
+    );
+
 }
